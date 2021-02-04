3a64d8df6e789cb25bfa67e3550b8b1f7f493a98
Rishabh Bhambani <bhambanirishabh@protonmail.com>
2021-02-04 17:25:46 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-// import Person from './Person/person.js'
-import Person2 from "./Person2/Person2.js";
+import Listitem from "./Listitem/Listitem.js";
 
 function App() {
   const [state, setState] = useState({
@@ -9,11 +8,34 @@ function App() {
     currentItem: { text: "", key: "" },
   });
 
+  const addItem = (e) => {
+    const newItem = state.currentItem.text;
+    if (newItem != "") {
+      setState({
+        items: [...state.items, newItem],
+        currentItem: {
+          text: "",
+          key: "",
+        },
+      });
+    }
+  };
+
+  const handleInput = (e) => {
+    setState({
+      items: [],
+      currentItem: { text: e.target.value, key: Date.now() },
+    });
+  };
+
   return (
-    <form onSubmit={addItem}>
-      <input type="text" onChange={handleInput} />
-      <button type="submit"></button>
-    </form>
+    <div className="App">
+      <form onSubmit={addItem}>
+        <input type="text" onChange={handleInput} />
+        <button type="submit">Add</button>
+      </form>
+      <Listitem items={state.items} />
+    </div>
   );
 }
 
---
+++ src/Listitem/Listitem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Listitem = (props) => {
+  const items = props.items;
+  console.log(items);
+  const ListItem = items.map((item) => {
+    return (
+      <div className="listitem" key="item.key">
+        <p>{item.text}</p>
+      </div>
+    );
+  });
+  return <div className="allList">{ListItem}</div>;
+};
+
+export default Listitem;
