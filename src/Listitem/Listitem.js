e49c5b248b5966fc29089343f624d8e0061ec7cb
Rishabh Bhambani <bhambanirishabh@protonmail.com>
2021-02-06 18:06:29 +0530

    Raw completion

--- src/Listitem/Listitem.js
+++ src/Listitem/Listitem.js
@@ -1,12 +1,11 @@
 import React from "react";
-
+import "../Listitem/Listitem.css";
 const Listitem = (props) => {
   const items = props.items;
-  console.log(items);
   const ListItem = items.map((item) => {
     return (
       <div className="listitem" key={item.key}>
-        <p>{item.text}</p>
+        <p>{item}</p>
       </div>
     );
   });
